31993c694030b0a5048aaf5e22a922ef32d5edd6
Slavo Kozar <slavo.kozar@gmail.com>
2020-05-11 20:26:02 +0200

    added Banner Large component

---
+++ dist/BannerLarge.js
@@ -0,0 +1,115 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.BannerLarge = BannerLarge;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Footer = require("./Footer");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function BannerLarge(props) {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "wrap"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    id: "grid-content",
+    className: "content"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    id: "section1",
+    className: "grid-section grid-section-bright"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "container grid-container"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 grid-cell grid-cell-content"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-component"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-content grid-heading editable-content",
+    "data-grid-content": "heading"
+  }, /*#__PURE__*/_react["default"].createElement("h1", {
+    className: " grid-align-left"
+  }, "Large Banner")))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-29 grid-cell grid-cell-column"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 grid-cell grid-cell-content"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-component"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-content grid-image  grid-align-center",
+    "data-grid-content": "image"
+  }, /*#__PURE__*/_react["default"].createElement("img", {
+    className: "img-fluid",
+    src: "https://klikem.cz/img/grid-dnd.png"
+  }))))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  }))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-31 grid-cell grid-cell-column"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 grid-cell grid-cell-content"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-component"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-content grid-text editable-content clearfix  ",
+    "data-grid-content": "text"
+  }, /*#__PURE__*/_react["default"].createElement("p", {
+    className: "grid-align-justify"
+  }, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."), /*#__PURE__*/_react["default"].createElement("p", null, "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."), /*#__PURE__*/_react["default"].createElement("p", null, "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."))))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 grid-cell grid-cell-content"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-component"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "grid-content grid-button grid-align-left ",
+    "data-grid-content": "button"
+  }, /*#__PURE__*/_react["default"].createElement("a", {
+    href: "#",
+    target: "_self",
+    className: "btn btn-lg btn-br-sm btn-inv "
+  }, "Chci v\u011Bd\u011Bt v\xEDce!"))))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })))), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })), /*#__PURE__*/_react["default"].createElement("div", {
+    className: "row grid-row"
+  }, /*#__PURE__*/_react["default"].createElement("div", {
+    className: "col-md-60 col-xs-60 grid-cell grid-cell-empty"
+  })))))), /*#__PURE__*/_react["default"].createElement(_Footer.Footer, null));
+}
--- dist/Preview.js
+++ dist/Preview.js
@@ -11,12 +11,16 @@ var _reactRouterDom = require("react-router-dom");
 
 var _Homepage = require("./Homepage");
 
+var _BannerLarge = require("./BannerLarge");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function Preview() {
   return /*#__PURE__*/_react["default"].createElement(_reactRouterDom.BrowserRouter, null, /*#__PURE__*/_react["default"].createElement(_reactRouterDom.Switch, null, /*#__PURE__*/_react["default"].createElement(_reactRouterDom.Route, {
     path: "/"
-  }, /*#__PURE__*/_react["default"].createElement(_Homepage.Homepage, null))));
+  }, /*#__PURE__*/_react["default"].createElement(_Homepage.Homepage, null)), /*#__PURE__*/_react["default"].createElement(_reactRouterDom.Route, {
+    path: "/banner-large"
+  }, /*#__PURE__*/_react["default"].createElement(_BannerLarge.BannerLarge, null))));
 }
 
 var _default = Preview;
