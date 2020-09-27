20ca15bce70f394305fe3e461e306bd73f88c5f8
Slavo Kozar <slavo.kozar@gmail.com>
2020-09-27 22:27:43 +0200

    another webpack config

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
+
 import {Homepage} from "./Homepage";
 import {BannerLarge} from "./BannerLarge";
 import Elements from "./Elements/Elements";
--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,57 @@ module.exports = {
     mode: 'production',
     entry: './src/components/Preview.js',
     output: {
-        path: path.resolve('lib'),
-        // filename: 'Preview.js',
-        libraryTarget: 'commonjs2'
+        path: path.join(__dirname, './lib'),
+        filename: 'myUnflappableComponent.js',
+
+        publicPath: '/dist/',
+        umdNamedDefine: true
+        // path: path.resolve('lib'),
+        // // filename: 'Preview.js',
+        // libraryTarget: 'commonjs2'
     },
+
     module: {
         rules: [
             {
-                test: /\.js?$/,
-                exclude: /(node_modules)/,
-                use: 'babel-loader'
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env']
+                    }
+                }
+                include: path.resolve(__dirname, "src"),
+                exclude: /node_modules/
             }
         ]
+    },
+    resolve: {
+        alias: {
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+            'react-router-dom': path.resolve(__dirname, './node_modules/react-router-dom'),
+        }
+    },
+    externals: {
+        // Don't bundle react or react-dom
+        react: {
+            commonjs: "react",
+            commonjs2: "react",
+            amd: "React",
+            root: "React"
+        },
+        "react-dom": {
+            commonjs: "react-dom",
+            commonjs2: "react-dom",
+            amd: "ReactDOM",
+            root: "ReactDOM"
+        },
+        "react-router-dom": {
+            commonjs: "react-router-dom",
+            commonjs2: "react-router-dom",
+            amd: "ReactRouterDOM",
+            root: "ReactRouterDOM"
+        },
     }
 }
